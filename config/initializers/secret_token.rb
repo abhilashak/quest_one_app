92f22c33bfd2d834446ece5f4e98da15ecc5725b
Abhilash.A.K <abhilash4892@gmail.com>
2010-09-28 10:15:36 +0530

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+QuestOneApp::Application.config.secret_token = 'a30a1d2139fd7b0bb190cfd83b8075974c3a10314a2b2ef6471ee8ccf03254e0fede2774ff9dd65b240473e37e4761e861521b03655e2dfa2df0aed713b1fcad'
